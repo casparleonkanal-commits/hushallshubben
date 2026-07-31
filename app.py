a9ef45382333e5bb364c794b943c5afc648c3e3f
caspar124 <caspar.lo.leon@gmail.com>
2026-08-01 00:08:49 +0200

    notiser

--- app.py
+++ app.py
@@ -126,6 +126,7 @@ def add():
     if assigned_to:
         # Skicka pushnotis till den användaren
         send_push_notification(
+            user_id=assigned_to,
             message=f"Du har fått sysslan: {title}"
         )
         
